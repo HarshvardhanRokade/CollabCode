197b31ba731452a068e6fd34cb07e5701ab811d3
Harshvardhan <harshvardhanrokade845@gmail.com>
2026-06-29 12:37:55 +0530

    ...

--- CollabCode/CollabCode.API/obj/Debug/net10.0/CollabCode.API.AssemblyInfo.cs
+++ CollabCode/CollabCode.API/obj/Debug/net10.0/CollabCode.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollabCode.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb04fba91aad661383cfca985bb9dfa97141f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4142ae3e2fd901fc456ebc9e32de3804abcc09b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollabCode.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollabCode.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
